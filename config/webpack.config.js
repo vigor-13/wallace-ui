e7b43cea6f19030f4014fee2c5703befa7d98a32
unknown <kkw5796@gmail.com>
2021-11-27 16:34:40 +0900

    feat: webpack-dev-server, thml-webpack-plugin 추가

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   mode: 'development',
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    })
+  ]
 };
