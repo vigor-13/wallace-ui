7917a242b69fc7eedfe9c5ffb0ef6718722758f4
unknown <kkw5796@gmail.com>
2021-12-02 13:55:42 +0900

    feat: jest 설치 및 설정

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ]
+};
