7d3716fbf7843141c98da013a4ba599a63b866f0
Decoy5796 <kkw5796@gmail.com>
2022-01-05 02:53:07 +0900

    feat: react 패키지에 button, layout 패키지 추가

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,6 @@
+export * from '@wallace-ui/system';
+export * from '@wallace-ui/theme';
+export * from '@wallace-ui/reset-css';
+export * from '@wallace-ui/layout';
+export * from '@wallace-ui/button';
 export * from './wallace-provider';
